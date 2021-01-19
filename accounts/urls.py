0311837981d5f43bb606e487c7932db75d83bce1
uibeom96 <dmlqja7570@daum.net>
2021-01-19 14:13:20 +0900

    accounts 회원가입 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path("login/", auth_view.LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path("logout/", auth_view.LogoutView.as_view(), name="logout")
+    path("logout/", auth_view.LogoutView.as_view(), name="logout"),
+    path("signup/", views.signup, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from accounts.forms import SignupForm
+from users.models import User
 
-# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            del(form.cleaned_data["password2"])
+            User.objects.create_user(**form.cleaned_data)
+            return redirect("shop:products_list")
+    else:
+        form = SignupForm()
+    return render(request, "accounts/signup.html", {"form": form})
